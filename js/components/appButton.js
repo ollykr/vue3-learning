02e2ead5ff70d5eb649dfe7e5f6b03a9911188ca
ollykr <okremianski@gmail.com>
2022-08-16 15:38:14 -0400

    Use props to set different bg colors and loading state for a button

--- js/components/appButton.js
+++ js/components/appButton.js
@@ -3,17 +3,34 @@ export default {
 	// template property
 	// Your component's html for "app-button" is going there
 	// We are "sloting" 'Submit' into a template
+	// base CSS classes are being set via an object which value is an boolean. If the boolean is set to 'true', we apply all the classes to the object, otherwise is set to 'false' and no classes applied
+	// Unique CSS classes being applied depending on styles of button
+	// e.g grey bg and dark grey bg on hover applied if type is 'muted'
+	// 'processing' prop is being referenced via 'processing' , we use css 'is-loading' class to apply the prop
 	template: `
-							<button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-5 disabled:cursor-not-allowed" :disabled="processing">
+							<button :class="{
+                                'border rounded px-5 py-5 disabled:cursor-not-allowed': true,
+                                'bg-blue-600 hover:bg-blue-700': type === 'primary',
+                                'bg-purple-200 hover:bg-purple-400': type === 'secondary',
+                                'bg-gray-200 hover:bg-gray-400': type === 'muted',
+                                'is-loading': processing
+                            }" :disabled="processing">
 
 								<slot/>
 
 							</button>
 						`,
-	data() {
-		// use case scenario: click on button submit, fetch request is sent to get data, 'processing' is set to true, while it is processing , the button is disabled , cursor not allowed, once fetch request is completed, change 'processing' to false
-		return {
-			processing: false,
-		};
+	// Passing props
+	props: {
+		// prop type is String
+		type: {
+			type: String,
+			default: "primary",
+		},
+		// set a state of the button from an "outside world"
+		processing: {
+			type: Boolean,
+			default: false,
+		},
 	},
 };
