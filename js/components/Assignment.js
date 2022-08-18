876c53646a1eee3becff90d1de18e987b22e431e
ollykr <okremianski@gmail.com>
2022-08-18 14:51:47 -0400

    Episode-8-handle-a-form-submission

--- js/components/Assignment.js
+++ js/components/Assignment.js
@@ -4,9 +4,9 @@ export default {
 					<!-- Filter down to only incompleted tasks and display them in In progress list -->
 					<!-- Whenever you loop over something (with "for") you always need to add a key, it has to be unique to the current item, e.g coming from DB  -->
 					<li>
-						<label>
+						<label class="p-2 flex justify-between items-center">
 							{{ assignment.name}}
-							<input type="checkbox" v-model="assignment.complete" />
+							<input type="checkbox" v-model="assignment.complete" class="ml-3" />
 						</label>
 					</li>
 
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -2,10 +2,18 @@ import AssignmentList from "./AssignmentList.js";
 // Outer assignments shell component
 export default {
 	components: { AssignmentList },
+	// .prevent is Vue's way to do e.preventDefault();
 	template: `
 	<section class="space-y-6">
 			<assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
 			<assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+
+			<form @submit.prevent="add">
+				<div class="border border-gray-600 text-black">
+					<input v-model="newAssignment" placeholder="New assignment..." class="p-2" />
+					<button type="submit" class="bg-white p-2 border-l">Add</button>
+				</div>
+			</form>
     </section>
     `,
 	data() {
@@ -15,6 +23,8 @@ export default {
 				{ name: "Read chapter 4", complete: false, id: 2 },
 				{ name: "Turn in homework", complete: false, id: 3 },
 			],
+			// Instead of diving into DOM with .querySelector etc we use below property as our only source of truth
+			newAssignment: "",
 		};
 	},
 	// Computed properties - are great for DRY principle to prevent code duplications
@@ -30,6 +40,19 @@ export default {
 				// adding display oldests filters in addition to inProgress / completed
 				// oldest: etc
 			};
+		},
+	},
+	methods: {
+		add() {
+			// alert(this.newAssignment);
+			// append a new assignment to assignments array
+			this.assignments.push({
+				name: this.newAssignment,
+				completed: false,
+				id: this.assignments.length + 1,
+			});
+			// to clear a new assignment value typed into an input box after "adding" it to assignments list
+			this.newAssignment = "";
 		},
 	},
 };
